834a4ff7d76c9766e87a7a7c3f3be77f424a9e41
Sekaia <gillotte.bm@gmail.com>
2022-08-10 14:22:25 -0500

    Appending all instances into a list

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Item:
+    pay_rate = 0.8  # The pay rate after 20% discount
+    all = []
+
     def __init__(self, name: str, price: float, quantity):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than zero!"
@@ -9,15 +12,24 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def __str__(self):
         return self.name
 
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item("Laptop", 1000, 2)
-item2 = Item("Phone", 100, 4)
 
-print(item1)
-print(item2)
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+for instance in Item.all:
+    print(instance.name)
