19c30c683fc1365427271d5f722d25528e444d6d
Sekaia <gillotte.bm@gmail.com>
2022-08-10 20:37:40 -0500

    Added price property

--- Item.py
+++ Item.py
@@ -12,11 +12,21 @@ class Item:
 
         # Assign to self object
         self.__name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         # Actions to execute
         Item.all.append(self)
+
+    @property
+    def price(self):
+        return self.__price
+
+    def apply_discount(self):
+        self.__price = self.__price * self.pay_rate
+
+    def apply_increment(self, increment_value):
+        self.__price = self.__price + self.__price * increment_value
 
     @property
     # Property Decorator = Read-Only Attribute
@@ -25,16 +35,16 @@ class Item:
 
     @name.setter
     def name(self, value):
-        self.__name = value
+        if len(value) > 10:
+            raise Exception("The name is too long!")
+        else:
+            self.__name = value
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.__price}, {self.quantity})"
 
     def calculate_total_price(self):
-        return self.price * self.quantity
-
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
+        return self.__price * self.quantity
 
     @classmethod
     def instantiate_from_csv(cls):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Item import Item
 
 item1 = Item("MyItem", 750)
+item1.apply_increment(0.2)
+item1.apply_discount()
 
-item1.name = "OtherItem"
-
-print(item1.name)
+item1.send_email()
